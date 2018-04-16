97dffe603e2809ffcd1f89d1a7a7923c6abbf200
sudeep shrestha <sudeep494@gmail.com>
2018-04-16 15:12:14 -0500

    decontext

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer } from '@angular/core';
+declare var window: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	constructor(private el: ElementRef, private renderer: Renderer) {
+	}
+	onMenuClick() {
+		if (window.innerWidth <992){
+			this.el.nativeElement.querySelector('.navbar-toggler')[0].click();
+		}
+	}
+
+	title = 'app';
 }
