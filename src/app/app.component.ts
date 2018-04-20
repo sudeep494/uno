c50b2ff7d5dfe80074f25f4f284d8f50e6f1b96b
sudeep shrestha <sudeep494@gmail.com>
2018-04-20 13:57:33 -0500

    cordova files excludes the platform and www folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var device;
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+	ngOnInit() {
+		document.addEventListener("deviceready", function() {
+			console.log(device.platform);
+		}, false);
+	}
 
 	isCollapsed = true;
 
